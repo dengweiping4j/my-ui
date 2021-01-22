d008ab7f394ff14dae682af9c54672772e342574
dengweiping4j <weipingdeng@qq.com>
2021-01-22 13:59:21 +0800

    样式布局调整

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import Link from 'umi/link';
-import { formatMessage } from 'umi/locale';
-import styles from './index.less';
+import styles from './index.less'
 
 export default () => (
-  <div className={styles.main} style={{ height: '100vh', width: '100%' }}>
-    <div className={styles.section}>
-      <span>404</span>
-      <p>
-        {formatMessage({ id: 'app.exception.description.404' })}
-        <Link to='/'> {formatMessage({ id: 'app.exception.back' })}</Link>
-      </p>
+  <div className={styles['notfound']} style={{ }}>
+    <div style={{ textAlign: 'center' }}>
+      <div><span style={{ fontSize: '60px', color: '#fff', fontFamily: '微软雅黑' }}>404 页面走丢了</span></div>
+      <img src={'/images/404.svg'} width={'50%'} style={{ margin: '40px' }} />
+      <div><a style={{ fontSize: '20px' }} href={'/'}>返回首页</a></div>
     </div>
   </div>
-
 );
 
